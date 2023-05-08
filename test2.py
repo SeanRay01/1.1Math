206f5aa137f79fd0533abd4a2b97178149d6f7ee
SeanRay <seantnt4@gmail.com>
2023-05-08 20:57:25 +0800

    '23/05/08'

--- test2.py
+++ test2.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 pi=np.pi
+# 建立 3D 圖形
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1, projection='3d')
 def sin(t):
     return(np.round(np.sin(t/180*pi),9))
 def cos(t):
@@ -36,20 +39,20 @@ def DH(i,a,alpha,d,theta):
                      [0,sin(alpha[i]),cos(alpha[i]),d[i]],
                      [0,0,0,1]]))
 def SixAxisModel(a_list,alpha,d_list,theta):
-    t00=DQ(0,0,0)
-    t01=DH(1,a_list,alpha,d_list,theta)
-    t12=DH(2,a_list,alpha,d_list,theta)
-    t02=t01*t12
-    t23=DH(3,a_list,alpha,d_list,theta)
-    t03=t02*t23
-    t34=DH(4,a_list,alpha,d_list,theta)
-    t04=t03*t34
-    t45=DH(5,a_list,alpha,d_list,theta)
-    t05=t04*t45
-    t56=DH(6,a_list,alpha,d_list,theta)
-    t06=t05*t56
-    t=[t00,t01,t02,t03,t04,t05,t06]
-    return(t)
+    t1=[]
+    t2=[DQ(0,0,0)]
+    for i in range(1,7):
+        t1.append(DH(i,a_list,alpha,d_list,theta))
+    data=np.mat([[1,0,0,0],
+                [0,1,0,0],
+                [0,0,1,0],
+                [0,0,0,1]])
+    print('-'*10,'t01','-'*10,'\n',data)
+    for i,j in enumerate(t1):
+        data=data*j
+        print('-'*10,f't0{i+2}','-'*10,'\n',data)
+        t2.append(data)
+    return(t2)
 def inverse_kinematics(a_list,d_list,t06):
     theta=[0,1,2,3,4,5,6]
     o=[0,t06[0,3],t06[1,3],t06[2,3]]
@@ -66,26 +69,6 @@ def inverse_kinematics(a_list,d_list,t06):
     theta[4]=atan2(r[1][3]*cos(theta[1])*cos(theta[2]+theta[3])+r[2][3]*sin(theta[1])*cos(theta[2]+theta[3])+r[3][3]*sin(theta[2]+theta[3]),-r[1][3]*cos(theta[1])*cos(theta[2]+theta[3])-r[2][3]*sin(theta[1])*sin(theta[2]+theta[3])+r[3][3]*cos(theta[2]+theta[3]))
     theta[6]=atan2(-r[1][1]*sin(theta[1])+r[2][1]*cos(theta[1]),r[1][2]*sin(theta[1])-r[2][2]*cos(theta[1]))
     return(theta)
-# DH_Model
-a_list = [0,750,750,0,0,0]
-alpha =  [90,0,90,-90,90,0]
-d_list = [1500,0,0,0,0,500]
-thetas = [90,30,30,0,80,0]
-t06=SixAxisModel(a_list,alpha,d_list,thetas)[6]
-print(t06)
-theta=inverse_kinematics(a_list,d_list,t06)
-print(theta)
-# 建立 3D 圖形
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1, projection='3d')
-#繪製坐標軸
-x=np.linspace(-2000, 2000, 100)
-y=np.linspace(0,0, 100)
-z=np.linspace(0,3500, 100)
-ax.plot(x,y,y, color='red')   #畫出三軸資料所構成的曲面
-ax.plot(y,x,y, color='red')   #畫出三軸資料所構成的曲面
-ax.plot(y,y,z, color='red',label='coordinate')   #畫出三軸資料所構成的曲面
-#繪製手臂位置
 def plot_dot(t1,t2):
     x=np.linspace(t1[0,3],t2[0,3], 2)
     y=np.linspace(t1[1,3],t2[1,3], 2)
@@ -93,10 +76,27 @@ def plot_dot(t1,t2):
     ax.plot(x,y,z)
     # print(x,y,z)
     ax.scatter(x,y,z, c=z, cmap='jet')
-t=SixAxisModel(a_list,alpha,d_list,thetas)
-for i in range(6):
-    plot_dot(t[i],t[i+1])
-# 顯示圖例
-ax.legend()
-# 顯示圖形
-plt.show()
+def plot(t):
+    #繪製坐標軸
+    x=np.linspace(-2000, 2000, 100)
+    y=np.linspace(0,0, 100)
+    z=np.linspace(0,3500, 100)
+    ax.plot(x,y,y, color='red')   #畫出三軸資料所構成的曲面
+    ax.plot(y,x,y, color='red')   #畫出三軸資料所構成的曲面
+    ax.plot(y,y,z, color='red',label='coordinate')   #畫出三軸資料所構成的曲面
+    #繪製手臂位置
+    for i in range(6):
+        plot_dot(t[i],t[i+1])
+    # 顯示圖例
+    ax.legend()
+    # 顯示圖形
+    plt.show()
+# DH_Model
+a_list = [0,750,750,0,0,0]#參數
+alpha =  [90,0,0,-90,90,0]#參數
+d_list = [1500,0,0,0,0,500]#參數
+thetas = [0,0,0,0,0,0]#參數
+t=SixAxisModel(a_list,alpha,d_list,thetas)#計算DH-Model
+theta=inverse_kinematics(a_list,d_list,t[6])#計算逆運動學
+print('\n\tinverse_kinematics\n',theta)
+plot(t)#繪圖
